9e15e416f3c93aa18f0ad35154fd8c99f50fd446
Nickanda <songyang20092009@gmail.com>
2021-03-27 01:24:09 +0000

    create login page

---
+++ src/layouts/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Nuxt />
+    </v-main>
+
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Footer from "../components/Footer";
+
+export default {
+  name: "App",
+
+  components: {
+    Footer,
+  },
+
+  data: () => ({}),
+};
+</script>
---
+++ src/pages/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container fluid>
+    <v-row class="col-7">
+      <v-img
+        src="/landing-background.jpg"
+        class="col-7 d-xs-none d-sm-none"
+        style="height: 95vh"
+      />
+    </v-row>
+    <v-row class="col-5">
+      <div class="text-h1">Sign in with your Google Account.</div>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "login",
+  layout: "login",
+
+  data: () => ({}),
+};
+</script>
