01335e1e21ec03371a39d4ea6dc367c3edc051da
Nickanda <songyang20092009@gmail.com>
2021-03-28 02:34:29 +0000

    add back cache

---
+++ src/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <NavBar />
+
+    <v-main>
+      <Nuxt />
+    </v-main>
+
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+
+export default {
+  name: "App",
+
+  components: {
+    Footer,
+    NavBar,
+  },
+
+  data: () => ({}),
+};
+</script>
